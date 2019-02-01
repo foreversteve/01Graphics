9daec8aec7e0d7693b4ad2a711711106d4d67dc6
Steven Liu <hliu4@stuy.edu>
2019-01-31 19:26:30 -0500

    Added Files

---
+++ picmaker.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+import sys
+
+def generate_picture():
+	fout = open("picture.ppm","w+")
+	fout.write("P3\n500 500\n255\n")
+
+	line = []
+	for i in range(500):
+		for j in range(500):
+			line.append(str(i//2)+" 255 "+str(j//2))
+		line.append("\n")
+		temp = " ".join(line)
+		fout.write(temp)
+		line = []
+
+generate_picture()
+
+
